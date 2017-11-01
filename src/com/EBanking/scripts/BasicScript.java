42a4a1e62e71bf5a77986c4e364ee6473f1b04ca
unknown <santhumiryala@gmail.com>
2017-11-01 11:40:44 +0530

    selprog

---
+++ src/com/EBanking/scripts/BasicScript.java
@@ -0,0 +1,24 @@
+package com.EBanking.scripts;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.firefox.FirefoxDriver;
+
+public class BasicScript {
+
+	public static void main(String[] args) 
+	{
+	
+		//Firefox Browser
+		
+		WebDriver driver=new FirefoxDriver();
+		
+		//Url
+		driver.get("http://183.82.100.55/ranford2/home.aspx");
+	 driver.findElement(By.id("txtuId")).sendKeys("Admin");
+	 driver.findElement(By.id("txtPword")).sendKeys("Admin");
+	 driver.findElement(By.xpath(".//*[@id='login']")).click();
+
+	}
+
+}
